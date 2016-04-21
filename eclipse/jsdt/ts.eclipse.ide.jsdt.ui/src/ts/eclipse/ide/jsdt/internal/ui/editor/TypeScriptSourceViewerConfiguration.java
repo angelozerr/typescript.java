0671ea5aad5ae05877bda8b36d8b829e4e8f5167
angelozerr <angelozerr@gmail.com>
2016-04-21 14:34:28 +0200

    Bind Ctrl+O to future quick outline. See https://github.com/angelozerr/typescript.java/issues/51

--- eclipse/jsdt/ts.eclipse.ide.jsdt.ui/src/ts/eclipse/ide/jsdt/internal/ui/editor/TypeScriptSourceViewerConfiguration.java
+++ eclipse/jsdt/ts.eclipse.ide.jsdt.ui/src/ts/eclipse/ide/jsdt/internal/ui/editor/TypeScriptSourceViewerConfiguration.java
@@ -12,12 +12,20 @@ package ts.eclipse.ide.jsdt.internal.ui.editor;
 
 import org.eclipse.jface.dialogs.IDialogSettings;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.text.AbstractInformationControlManager;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IInformationControl;
+import org.eclipse.jface.text.IInformationControlCreator;
 import org.eclipse.jface.text.contentassist.ContentAssistant;
 import org.eclipse.jface.text.contentassist.IContentAssistProcessor;
 import org.eclipse.jface.text.contentassist.IContentAssistant;
 import org.eclipse.jface.text.formatter.IContentFormatter;
+import org.eclipse.jface.text.information.IInformationPresenter;
+import org.eclipse.jface.text.information.IInformationProvider;
+import org.eclipse.jface.text.information.InformationPresenter;
 import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.texteditor.ITextEditor;
 import org.eclipse.wst.jsdt.internal.ui.JavaScriptPlugin;
 import org.eclipse.wst.jsdt.internal.ui.text.ContentAssistPreference;
@@ -29,6 +37,9 @@ import org.eclipse.wst.jsdt.ui.text.JavaScriptSourceViewerConfiguration;
 
 import ts.eclipse.ide.jsdt.internal.ui.editor.contentassist.TypeScriptCompletionProcessor;
 import ts.eclipse.ide.jsdt.internal.ui.editor.format.TypeScriptContentFormatter;
+import ts.eclipse.ide.jsdt.ui.actions.ITypeScriptEditorActionDefinitionIds;
+import ts.eclipse.ide.ui.outline.TypeScriptElementProvider;
+import ts.eclipse.ide.ui.outline.TypeScriptQuickOutlineDialog;
 import ts.eclipse.ide.ui.utils.EditorUtils;
 
 /**
@@ -102,5 +113,70 @@ public class TypeScriptSourceViewerConfiguration extends JavaScriptSourceViewerC
 	@Override
 	public IContentFormatter getContentFormatter(ISourceViewer sourceViewer) {
 		return new TypeScriptContentFormatter(EditorUtils.getResource(getEditor()));
+	}
+
+	/**
+	 * Returns the outline presenter which will determine and shown information
+	 * requested for the current cursor position.
+	 *
+	 * @param sourceViewer
+	 *            the source viewer to be configured by this configuration
+	 * @param doCodeResolve
+	 *            a boolean which specifies whether code resolve should be used
+	 *            to compute the JavaScript element
+	 * @return an information presenter
+	 *
+	 */
+	@Override
+	public IInformationPresenter getOutlinePresenter(final ISourceViewer sourceViewer, final boolean doCodeResolve) {
+		InformationPresenter presenter = null;
+		if (doCodeResolve) {
+			// presenter = new
+			// InformationPresenter(getOutlinePresenterControlCreator(sourceViewer,
+			// ITypeScriptEditorActionDefinitionIds.OPEN_STRUCTURE));
+			return null;
+		} else {
+			presenter = new InformationPresenter(
+					getOutlinePresenterControlCreator(sourceViewer, ITypeScriptEditorActionDefinitionIds.SHOW_OUTLINE));
+		}
+		presenter.setDocumentPartitioning(getConfiguredDocumentPartitioning(sourceViewer));
+		presenter.setAnchor(AbstractInformationControlManager.ANCHOR_GLOBAL);
+		IInformationProvider provider = new TypeScriptElementProvider(getEditor());
+		presenter.setInformationProvider(provider, IDocument.DEFAULT_CONTENT_TYPE);
+		presenter.setInformationProvider(provider, IJavaScriptPartitions.JAVA_DOC);
+		presenter.setInformationProvider(provider, IJavaScriptPartitions.JAVA_MULTI_LINE_COMMENT);
+		presenter.setInformationProvider(provider, IJavaScriptPartitions.JAVA_SINGLE_LINE_COMMENT);
+		presenter.setInformationProvider(provider, IJavaScriptPartitions.JAVA_STRING);
+		presenter.setInformationProvider(provider, IJavaScriptPartitions.JAVA_CHARACTER);
+		presenter.setSizeConstraints(50, 20, true, false);
+		return presenter;
+	}
+
+	/**
+	 * Returns the outline presenter control creator. The creator is a factory
+	 * creating outline presenter controls for the given source viewer. This
+	 * implementation always returns a creator for
+	 * <code>JavaOutlineInformationControl</code> instances.
+	 *
+	 * @param sourceViewer
+	 *            the source viewer to be configured by this configuration
+	 * @param commandId
+	 *            the ID of the command that opens this control
+	 * @return an information control creator
+	 *
+	 */
+	private IInformationControlCreator getOutlinePresenterControlCreator(final ISourceViewer sourceViewer,
+			final String commandId) {
+		return new IInformationControlCreator() {
+			public IInformationControl createInformationControl(final Shell parent) {
+				int shellStyle = SWT.RESIZE;
+				try {
+					return new TypeScriptQuickOutlineDialog(parent, shellStyle,
+							((TypeScriptEditor) getEditor()).getTypeScriptFile());
+				} catch (Exception e) {
+					return null;
+				}
+			}
+		};
 	}
 }
