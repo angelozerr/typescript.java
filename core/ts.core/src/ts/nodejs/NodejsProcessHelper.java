528eb09af562bbafa7376d207daac9124649f32b
angelozerr <angelo.zerr@gmail.com>
2016-11-07 07:37:58 +0100

    Find node path with "which node" command.

--- core/ts.core/src/ts/nodejs/NodejsProcessHelper.java
+++ core/ts.core/src/ts/nodejs/NodejsProcessHelper.java
@@ -10,11 +10,16 @@
  */
 package ts.nodejs;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 import ts.OS;
+import ts.utils.IOUtils;
+import ts.utils.StringUtils;
 
 /**
  * Node path helper.
@@ -24,14 +29,12 @@ public class NodejsProcessHelper {
 
 	private static final String[] WINDOWS_NODE_PATHS = new String[] {
 			"C:/Program Files/nodejs/node.exe".replace('/', File.separatorChar),
-			"C:/Program Files (x86)/nodejs/node.exe".replace('/',
-					File.separatorChar), "node" };
+			"C:/Program Files (x86)/nodejs/node.exe".replace('/', File.separatorChar), "node" };
 
-	private static final String[] MACOS_NODE_PATHS = new String[] {
-			"/usr/local/bin/node", "/opt/local/bin/node", "node" };
+	private static final String[] MACOS_NODE_PATHS = new String[] { "/usr/local/bin/node", "/opt/local/bin/node",
+			"node" };
 
-	private static final String[] LINUX_NODE_PATHS = new String[] {
-			"/usr/local/bin/node", "node" };
+	private static final String[] LINUX_NODE_PATHS = new String[] { "/usr/local/bin/node", "node" };
 
 	private NodejsProcessHelper() {
 	}
@@ -94,7 +97,7 @@ public class NodejsProcessHelper {
 			}
 		}
 
-		return null;
+		return getNodeLocation(os);
 	}
 
 	private static String getNodeFileName(OS os) {
@@ -104,4 +107,34 @@ public class NodejsProcessHelper {
 		return "node";
 	}
 
+	/**
+	 * Returns the node.js location by using command "which node".
+	 * 
+	 * @param os
+	 * @return the node.js location by using command "which node".
+	 */
+	private static File getNodeLocation(OS os) {
+		String[] command = new String[] { "/bin/bash", "-c", "which node" };
+		if (os == OS.Windows) {
+			command = new String[] { "cmd", "/c", "where node" };
+		} else {
+			command = new String[] { "/bin/bash", "-c", "which node" };
+		}
+		BufferedReader reader = null;
+		try {
+			Process p = Runtime.getRuntime().exec(command);
+			reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String nodeFile = reader.readLine();
+			if (StringUtils.isEmpty(nodeFile)) {
+				return null;
+			}
+			File f = new File(nodeFile);
+			return f.exists() ? f : null;
+		} catch (IOException e) {
+			//e.printStackTrace();
+		} finally {
+			IOUtils.closeQuietly(reader);
+		}
+		return null;
+	}
 }
